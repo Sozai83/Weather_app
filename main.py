8de276501d2876e4df95fac373fecbe4c7b01adb
Sozai83 <chikushi3.227@gmail.com>
2023-11-03 22:15:26 +1100

    optimized html - render form radio option using for loop and the locations dictionary

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return  render_template('index.html')
+    return  render_template('index.html', locations=locations)
 
 
 @app.route('/checkweather', methods=['POST'])
@@ -48,15 +48,20 @@ class Geocode:
         self.location = location        
 
     def check_geocode(self):
-        resp = requests.get(f'{geocode_url}{self.location.replace(" ", "+")}&key={geocode_api_key}')
-        status = resp.json()['status']
-
-        if resp.status_code == 200 and status != 'ZERO_RESULTS':
-            self.latitude = resp.json()['results'][0]['geometry']['location']['lat']
-            self.altitude = resp.json()['results'][0]['geometry']['location']['lng']
-            print(self.latitude, self.altitude)
+        if self.location in locations:
+            self.latitude = locations[self.location]['lat']
+            self.altitude = locations[self.location]['alt']
+            
         else:
-            print(status)
+            resp = requests.get(f'{geocode_url}{self.location.replace(" ", "+")}&key={geocode_api_key}')
+            status = resp.json()['status']
+
+            if resp.status_code == 200 and status != 'ZERO_RESULTS':
+                self.latitude = resp.json()['results'][0]['geometry']['location']['lat']
+                self.altitude = resp.json()['results'][0]['geometry']['location']['lng']
+                print(self.latitude, self.altitude)
+            else:
+                print(status)
         
 
 
@@ -81,6 +86,65 @@ class Weather:
         else:
             print(resp.status_code)
 
+
+
+locations = {
+    'Lake District National Park': {
+        'id': 'cumbria',
+        'name': 'Lake District National Park',
+        'lat': 54.4609,
+        'alt': -3.0886
+    },
+    'the_cotswolds':{
+        'id': 'the_cotswolds',
+        'name': 'The Cotswolds',
+        'lat': 50.6395,
+        'alt': -2.0566
+    },
+    'cambridge':{
+        'id': 'cambridge',
+        'name': 'Cambridge ',
+        'lat': 52.2053,
+        'alt': 0.1218
+    },
+    'bristol':{
+        'id': 'bristol',
+        'name': 'Bristol',
+        'lat': 51.4545,
+        'alt': -2.5879
+    },
+    'oxford':{
+        'id': 'oxford',
+        'name': 'Oxford',
+        'lat': 51.7520,
+        'alt': -1.2577
+    },
+    'norwich':{
+        'id': 'norwich',
+        'name': 'Norwich',
+        'lat': 52.6309,
+        'alt': 1.2974
+    },
+    'stonehenge':{
+        'id': 'stonehenge',
+        'name': 'Stonehenge',
+        'lat': 51.1789,
+        'alt': -1.8262
+    },
+    'watergate_bay':{
+        'id': 'watergate_bay',
+        'name': 'Watergate Bay',
+        'lat': 50.4429,
+        'alt': -5.0553
+    },
+    'birmingham':{
+        'id': 'birmingham',
+        'name': 'Birmingham',
+        'lat': 52.4862,
+        'alt': -1.8904
+    }
+    
+}
 
 
 if __name__ == "__main__":
