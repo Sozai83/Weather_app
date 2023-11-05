0e3dfbee22da48f25620dc0dccac89461c7dc889
Sozai83 <chikushi3.227@gmail.com>
2023-11-05 19:04:10 +1100

    added comments

--- locations.py
+++ locations.py
@@ -3,55 +3,55 @@ locations = {
         'id': 'cumbria',
         'name': 'Lake District National Park',
         'lat': 54.4609,
-        'alt': -3.0886
+        'lng': -3.0886
     },
     'the_cotswolds':{
         'id': 'the_cotswolds',
         'name': 'The Cotswolds',
         'lat': 50.6395,
-        'alt': -2.0566
+        'lng': -2.0566
     },
     'cambridge':{
         'id': 'cambridge',
         'name': 'Cambridge ',
         'lat': 52.2053,
-        'alt': 0.1218
+        'lng': 0.1218
     },
     'bristol':{
         'id': 'bristol',
         'name': 'Bristol',
         'lat': 51.4545,
-        'alt': -2.5879
+        'lng': -2.5879
     },
     'oxford':{
         'id': 'oxford',
         'name': 'Oxford',
         'lat': 51.7520,
-        'alt': -1.2577
+        'lng': -1.2577
     },
     'norwich':{
         'id': 'norwich',
         'name': 'Norwich',
         'lat': 52.6309,
-        'alt': 1.2974
+        'lng': 1.2974
     },
     'stonehenge':{
         'id': 'stonehenge',
         'name': 'Stonehenge',
         'lat': 51.1789,
-        'alt': -1.8262
+        'lng': -1.8262
     },
     'watergate_bay':{
         'id': 'watergate_bay',
         'name': 'Watergate Bay',
         'lat': 50.4429,
-        'alt': -5.0553
+        'lng': -5.0553
     },
     'birmingham':{
         'id': 'birmingham',
         'name': 'Birmingham',
         'lat': 52.4862,
-        'alt': -1.8904
+        'lng': -1.8904
     }
     
 }
--- main.py
+++ main.py
@@ -14,20 +14,32 @@ def home():
     return  render_template('index.html', locations=locations)
 
 
+# Check weather for the selected location, and return data using weather.html template
 @app.route('/checkweather', methods=['POST'])
 def check_weather():
     if request.method == 'POST':
         try:
             location = request.form['location']
             unit = request.form['unit']
+
+            # Create a Geocode object
             geo_code = Geocode(location)
+            # Call check_geocode to retrieve latitude and longitude 
             geo_code.check_geocode()
 
-            if geo_code.latitude and geo_code.altitude:
-                weather = Weather(location, geo_code.latitude, geo_code.altitude,unit)
+            if geo_code.latitude and geo_code.longitude:
+                # Create a Weather object
+                weather = Weather(location, geo_code.latitude, geo_code.longitude,unit)
+
+                # calsls the check_weather funcion to check current weather and store the data in the object 
                 weather.check_weather()
+
+                # calsls the check_weather_forecast function to check weather in the location for next 8 days and store the data in the object
                 weather.check_weather_forecast()
+
+
                 return render_template('weather.html', 
+                                    location = location,
                                     weather = weather.weather,
                                     temp = weather.temp,
                                     temp_max = weather.temp_max,
@@ -35,15 +47,16 @@ def check_weather():
                                     humidity = weather.humidity,
                                     icon = weather.icon,
                                     date = weather.date,
-                                    location = location,
                                     unit = weather.unit,
                                     next_7days = weather.weather_next_7days,
                                     latitude = weather.latitude,
-                                    altitude = weather.altitude,
+                                    longitude = weather.longitude,
                                     map= weather.map
                                     )
+            # If the geocode cannot be found, return the error page
             else:
-                return 'Please select valid location'
+                return redirect(url_for('error'))
+        # If an error occurs, return the error page
         except:
             return redirect(url_for('error'))
         
@@ -53,11 +66,19 @@ def error():
     return render_template('error.html')
 
 
+# API Key for geocode_url
 geocode_api_key = os.environ.get('GoogleMapAPIKey')
+# API Key for map_url
 map_api_key = os.environ.get('GoogleMapAPIKeyLimited')
+# API Key for weather_url and weather_forecast_url
 weather_api_key = os.environ.get('OpenWeather_API_key')
+
+
+# Google map API - Retrieve longitude and latitude based on location
 geocode_url = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+# Google map API - Embed map API URL
 map_url = 'https://www.google.com/maps/embed/v1/place'
+# Open Weather API - 
 weather_url = 'https://api.openweathermap.org/data/2.5/weather'
 weather_forecast_url = 'https://api.openweathermap.org/data/3.0/onecall'
 
@@ -67,32 +88,35 @@ class Geocode:
         self.location = location        
 
     def check_geocode(self):
+        # If the location is in the existing location disctiornary, return latitude and longitude
         if self.location in locations:
             self.latitude = locations[self.location]['lat']
-            self.altitude = locations[self.location]['alt']
-            
+            self.longitude = locations[self.location]['lng']
+        
+        # Otherwise, retrieve latitude and longitude using google geocode API
         else:
             resp = requests.get(f'{geocode_url}{self.location.replace(" ", "+")}&key={geocode_api_key}')
             status = resp.json()['status']
 
             if resp.status_code == 200 and status != 'ZERO_RESULTS':
                 self.latitude = resp.json()['results'][0]['geometry']['location']['lat']
-                self.altitude = resp.json()['results'][0]['geometry']['location']['lng']
+                self.longitude = resp.json()['results'][0]['geometry']['location']['lng']
             else:
-                print(status)
+                return resp.status_code
         
 
 
 class Weather:
-    def __init__(self, location, latitude, altitude, unit='metric'):
+    def __init__(self, location, latitude, longitude, unit='metric'):
         self.location = location
         self.latitude = latitude
-        self.altitude = altitude
+        self.longitude = longitude
         self.unit = unit
-        self.map = f'{map_url}?key={map_api_key}&q={self.latitude},{self.altitude}'
+        self.map = f'{map_url}?key={map_api_key}&q={self.latitude},{self.longitude}'
     
+    # Get current weather for the location
     def check_weather(self):
-        resp = requests.get(f'{weather_url}?lat={self.latitude}&lon={self.altitude}&units={self.unit}&zoom=0&appid={weather_api_key}')
+        resp = requests.get(f'{weather_url}?lat={self.latitude}&lon={self.longitude}&units={self.unit}&zoom=0&appid={weather_api_key}')
         
         if resp.status_code == 200:
             resp_json = resp.json()
@@ -104,10 +128,11 @@ class Weather:
             self.icon = f"https://openweathermap.org/img/wn/{resp_json['weather'][0]['icon']}@2x.png"
             self.date = datetime.utcfromtimestamp(resp_json["dt"]).strftime('%Y/%m/%d %I%p')
         else:
-            print(f'{weather_url}?lat={self.latitude}&lon={self.altitude}&units={self.unit}&appid={weather_api_key}', esp.status_code)
+            print(resp.status_code)
 
+    # Get weather forecast for next 8 days for the location
     def check_weather_forecast(self):
-        resp = requests.get(f'{weather_forecast_url}?lat={self.latitude}&lon={self.altitude}&units={self.unit}&exclude=hourly,current,minutely,alerts&appid={weather_api_key}')
+        resp = requests.get(f'{weather_forecast_url}?lat={self.latitude}&lon={self.longitude}&units={self.unit}&exclude=hourly,current,minutely,alerts&appid={weather_api_key}')
         
         if resp.status_code == 200:
             weather_next_7days = map(lambda x: 
@@ -122,8 +147,7 @@ class Weather:
             self.weather_next_7days = list(weather_next_7days)
             
         else:
-            self.weather_next_7days = 'Unable to retrieve forecasts. Please refresh the page.'
-            print(resp.status_code)
+            return resp.status_code
 
 
 if __name__ == "__main__":
